a998942644b2ea18fef0826b365597164f305223
GracielaCanessa <gracane@gmail.com>
2020-08-31 16:47:14 -0300

    Comenzando a preparar carpetas asociadas a la eah2020.

---
+++ eah2020/estructura_eah2020.php
@@ -0,0 +1,12 @@
+<?php
+//UTF-8:SÍ
+require_once "lo_imprescindible.php";
+require_once "tabla_operativos.php";
+require_once "metadatos_eah2020.php";
+require_once "pdo_con_excepciones.php";
+require_once "nuestro_mini_yaml_parse.php";
+
+class Estructura_eah2020 extends Estructura_js{ 
+}
+
+?>
