74d3812d0e5d312e1a042d500a5d3a0136d6607b
GracielaCanessa <gracane@gmail.com>
2020-08-31 18:23:20 -0300

    cambiando nombre próximo operativo

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi203', 'Encuesta de Trabajo, Ocupación e Ingreso 2020 3','', true, false, 
+values ('eah2020', 'Encuesta Anual de Hogares 2020','', true, false, 
             null, 1 );
 
