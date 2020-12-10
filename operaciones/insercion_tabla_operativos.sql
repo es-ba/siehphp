d537adaac58d8d9513b5e22b013ea317f3fd2ece
GracielaCanessa <gracane@gmail.com>
2020-12-09 21:55:25 -0300

    Empezando a preparar archivos para instalación operativo etoi211.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('eah2020', 'Encuesta Anual de Hogares 2020','', true, false, 
+values ('etoi211', 'Encuesta de Trabajo, Ocupación e Ingreso 2021 1 ','', true, false, 
             null, 1 );
 
