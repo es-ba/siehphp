42185c4a796a607013b6fcab847045b6d062b0e7
GracielaCanessa <gracane@gmail.com>
2021-09-24 13:54:43 -0300

    Actualización versión por agregado de consistencias relevamiento 1 y 2.

--- eah2021/sw-manifest.js
+++ eah2021/sw-manifest.js
@@ -1,6 +1,6 @@
 "use strict";
 // TEMPLATE-START
-var version = 'v 3.03m';
+var version = 'v 3.03n';
 var appName = 'eah2021_test';
 var urlsToCache = [
 '../eah2021/eah2021_icon.png',
--- encuestas/encuestas.js
+++ encuestas/encuestas.js
@@ -1,7 +1,7 @@
 //UTF-8:SÍ
 "use strict";
 
-var version_js_encuestas='v 3.03m';
+var version_js_encuestas='v 3.03n';
 
 var operativo_actual;
 var anio_operativo;
