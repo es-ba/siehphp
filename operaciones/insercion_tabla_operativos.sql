ed4e6e50e7197dd941e8c624fb733c0fda734f22
GracielaCanessa <gracane@gmail.com>
2021-11-25 18:39:03 -0300

    continuando la preparación de Etoi221

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi213', 'Encuesta de Trabajo, Ocupación e Ingreso 2021 3','', true, false, 
+values ('etoi221', 'Encuesta de Trabajo, Ocupación e Ingreso 2022 1','', true, false, 
             null, 1 );
 
