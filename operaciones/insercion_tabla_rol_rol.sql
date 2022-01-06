25afc3434b8f7b6fd46c724b0ed309b35f47b5ec
GracielaCanessa <gracane@gmail.com>
2022-01-06 14:52:35 -0300

    cambio para selección aleatoria de casos a supervisar.

--- operaciones/insercion_tabla_rol_rol.sql
+++ operaciones/insercion_tabla_rol_rol.sql
@@ -1,3 +1,4 @@
+/* verificar que el operativo anterior tenga agregados para coor_campo y subcoor_campo los roles delegados  encuestador, supervisor y recuperador */
 INSERT INTO encu.rol_rol(
             rolrol_principal, rolrol_delegado, rolrol_tlg)
 SELECT rolrol_principal, rolrol_delegado, /*CAMPOS_AUDITORIA*/ rolrol_tlg
