6663ad006f9783704df251cfe5e34071bb7989ba
GracielaCanessa <gracane@gmail.com>
2022-03-03 15:16:44 -0300

    cambios para armar operativo preju_2022 impresión etiquetas.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi222', 'Encuesta de Trabajo, Ocupación e Ingreso 2022 2','', true, false, 
+values ('preju_2022', 'Prevalencia de Juegos 2022 para impresión etiquetas','', true, false, 
             null, 1 );
 
