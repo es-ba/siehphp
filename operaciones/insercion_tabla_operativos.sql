afa0c69a39ded3bdfcd0f4a3f162cecefb2d31a9
GracielaCanessa <gracane@gmail.com>
2022-06-01 13:23:18 -0300

    comenzando a preparar la etoi223.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('preju_2022', 'Prevalencia de Juegos 2022 para impresión etiquetas','', true, false, 
+values ('etoi223', 'Encuesta de Trabajo, Ocupación e Ingreso 2022 3','', true, false, 
             null, 1 );
 
