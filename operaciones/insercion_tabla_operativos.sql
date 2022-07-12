8222837344457fc4ae70b68479b4e5851cda7b85
GracielaCanessa <gracane@gmail.com>
2022-07-12 16:58:45 -0300

    Para Eah2022

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi223', 'Encuesta de Trabajo, Ocupación e Ingreso 2022 3','', true, false, 
+values ('eah2022', 'Encuesta Anual de Hogares 2022','', true, false, 
             null, 1 );
 
