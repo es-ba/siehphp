38494005d85fc287c4751e751e521ba51f987ea0
GracielaCanessa <gracane@gmail.com>
2022-10-28 16:53:10 -0300

    cambios para etoi231.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('eah2022', 'Encuesta Anual de Hogares 2022','', true, false, 
+values ('etoi231', 'Encuesta de Trabajo, Ocupación e Ingreso 2023 1','', true, false, 
             null, 1 );
 
