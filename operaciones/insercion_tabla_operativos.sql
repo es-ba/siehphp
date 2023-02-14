bd20c68df7dfd8ac2fbd7b96f93035c46685e8b9
GracielaCanessa <gracane@gmail.com>
2023-02-14 11:21:17 -0300

    continuando tareas de instalación etoi232.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('vcm2023', 'Violencia Contra la Mujer 2023','', true, false, 
+values ('etoi232', 'Encuesta de Trabajo, Ocupación e Ingreso 2023 2','', true, false, 
             null, 1 );
 
