0d82d7398532ffbff4c8b7e4c70f2af9115b9f88
GracielaCanessa <gracane@gmail.com>
2023-08-22 13:58:49 -0300

    actualizando para eah2023.

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,6 +1,6 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi233', 'Encuesta de Trabajo, Ocupación e Ingreso 2023 3','', true, false, 
+values ('eah2023', 'Encuesta Anual de Hogares 2023','', true, false, 
             null, 1 );
 
