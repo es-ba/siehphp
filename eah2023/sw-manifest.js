5c38e0b6601fade54c93f721880318c7746a71fa
GracielaCanessa <gracane@gmail.com>
2023-09-25 13:28:22 -0300

    Eah2023-actualizando versiones para próxima capacitación.

--- eah2023/sw-manifest.js
+++ eah2023/sw-manifest.js
@@ -1,6 +1,6 @@
 "use strict";
 // TEMPLATE-START
-var version = 'v 3.08';
+var version = 'v 3.09';
 var appName = 'eah2023_test';
 var urlsToCache = [
 '../eah2023/eah2023_icon.png',
--- encuestas/encuestas.js
+++ encuestas/encuestas.js
@@ -1,7 +1,7 @@
 //UTF-8:SÍ
 "use strict";
 
-var version_js_encuestas='v 3.08';
+var version_js_encuestas='v 3.09';
 
 var operativo_actual;
 var anio_operativo;
