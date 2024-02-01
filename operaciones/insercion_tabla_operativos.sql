170c0b8caa72154120c096677049bfe04d18a7f0
GracielaCanessa <gracane@gmail.com>
2024-02-01 11:16:04 -0300

    para nuevo operativo etoi242

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,5 +1,5 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('etoi241', 'Encuesta de Trabajo, Ocupación e Ingreso 2024 1','', true, false,null, 1 );
+values ('etoi242', 'Encuesta de Trabajo, Ocupación e Ingreso 2024 2','', true, false,null, 1 );
 
