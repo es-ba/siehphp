80f8e87c5728e6192c61652f8750a6ec5d51434a
GracielaCanessa <gracane@gmail.com>
2024-08-20 18:36:26 -0300

    modificando nombre operativo para etoi244

--- operaciones/insercion_tabla_operativos.sql
+++ operaciones/insercion_tabla_operativos.sql
@@ -1,5 +1,5 @@
 INSERT INTO encu.operativos(
             ope_ope, ope_nombre, ope_ope_anterior, ope_en_campo, ope_rev_metadatos, 
             ope_dispositivo_unico, ope_tlg)
-values ('eah2024', 'Encuesta Anual de Hogares 2024','', true, false,null, 1 );
+values ('etoi244', 'Encuesta de Trabajo, Ocupación e Ingreso 2024 4','', true, false,null, 1 );
 
