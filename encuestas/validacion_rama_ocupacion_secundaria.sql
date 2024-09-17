303d7df901cbfb5e31cf86fccedcaf17771a890a
Sebastián Pierini <seba.i.pierini@gmail.com>
2024-09-17 11:52:19 -0300

    se agrega validacion rama ocupacion

--- encuestas/validacion_rama_ocupacion_secundaria.sql
+++ encuestas/validacion_rama_ocupacion_secundaria.sql
@@ -61,3 +61,68 @@ BEFORE UPDATE
 ON encu.plana_i1_
 FOR EACH ROW
 EXECUTE FUNCTION encu.unir_rama_ocupacion_sec_trg();
+
+--------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION encu.validacion_rama_ocupacion_sec_trg()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+v_nota text;
+v_texto text;
+BEGIN
+select
+case when new.pla_rama1_sec is distinct from old.pla_rama1_sec then 
+        comun.validar_codrama(new.pla_rama1_sec, 1,2, 'rama1_sec')
+     when new.pla_rama2_sec is distinct from old.pla_rama2_sec then 
+        comun.validar_codrama(new.pla_rama2_sec,3,2,'rama2_sec')||comun.validar_codrama(new.pla_rama1_sec||new.pla_rama2_sec,1,4,'rama') 
+     when new.pla_ocu1_sec  is distinct from old.pla_ocu1_sec  then 
+        comun.validar_codocupacion(new.pla_ocu1_sec, 1,1,'ocu1_sec')
+     when new.pla_ocu2_sec  is distinct from old.pla_ocu2_sec  then
+        comun.validar_codocupacion(new.pla_ocu2_sec, 1,1,'ocu2_sec')||     
+        comun.validar_codocupacion(new.pla_ocu1_sec||new.pla_ocu2_sec, 1,2,'ocu2_sec-hasta 2 dig')
+     when new.pla_ocu3_sec  is distinct from old.pla_ocu3_sec  then 
+        comun.validar_codocupacion(new.pla_ocu3_sec, 1,1,'ocu3_sec')||     
+        comun.validar_codocupacion(new.pla_ocu1_sec||new.pla_ocu2_sec||new.pla_ocu3_sec, 1,3,'ocu3_sec-hasta 3 dig')
+     when new.pla_ocu4_sec  is distinct from old.pla_ocu4_sec  then 
+        comun.validar_codocupacion(new.pla_ocu4_sec, 1,1,'ocu4')||     
+        comun.validar_codocupacion(new.pla_ocu1_sec||new.pla_ocu2_sec||new.pla_ocu3_sec||new.pla_ocu4_sec, 1,4, 'ocu4_sec-hasta 4 dig')
+     when new.pla_ocu5_sec  is distinct from old.pla_ocu5_sec  then 
+        comun.validar_codocupacion(new.pla_ocu5_sec, 1,1,'ocu5_sec')||     
+        comun.validar_codocupacion(new.pla_ocu1_sec||new.pla_ocu2_sec||new.pla_ocu3_sec||new.pla_ocu4_sec||new.pla_ocu5_sec, 1,5, 'ocupacion')    
+     else ''
+end into v_nota;
+
+if (new.pla_ocu1_sec is distinct from old.pla_ocu1_sec or new.pla_ocu2_sec  is distinct from old.pla_ocu2_sec or new.pla_ocu3_sec is distinct from old.pla_ocu3_sec  or new.pla_ocu4_sec is distinct from old.pla_ocu4_sec or new.pla_ocu5_sec is distinct from old.pla_ocu5_sec) then  
+    if length(new.pla_ocu1_sec||new.pla_ocu2_sec||new.pla_ocu3_sec||new.pla_ocu4_sec||new.pla_ocu5_sec)=5 then
+        v_texto:=comun.validar_codocupacion(new.pla_ocu1_sec||new.pla_ocu2_sec||new.pla_ocu3_sec||new.pla_ocu4_sec||new.pla_ocu5_sec, 1,5, 'total ocupacion');         
+        if v_texto <> '' then
+            v_nota:=v_nota||v_texto;   
+        end if;
+    end if;        
+end if;    
+if (new.pla_rama1_sec is distinct from old.pla_rama1_sec or new.pla_rama2_sec is distinct from old.pla_rama2_sec) then  
+    if length(new.pla_rama1_sec||new.pla_rama2_sec)=4 then
+        v_texto:= comun.validar_codrama(new.pla_rama1_sec||new.pla_rama2_sec,1,4,'total rama') ;         
+        if v_texto <> '' then
+            v_nota:=v_nota||v_texto;   
+        end if;
+    end if;        
+end if;  
+  
+if v_nota <> '' then
+    raise ' %', v_nota;
+end if;    
+return new;
+
+END
+$BODY$
+  LANGUAGE plpgsql;
+ALTER FUNCTION encu.validacion_rama_ocupacion_sec_trg()
+  OWNER TO tedede_php;  
+  
+CREATE TRIGGER validacion_rama_ocupacion_sec_i1_trg
+  BEFORE UPDATE
+  ON encu.plana_i1_
+  FOR EACH ROW
+  EXECUTE PROCEDURE encu.validacion_rama_ocupacion_sec_trg(); 
