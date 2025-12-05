b16d6c1c2b058d845c5650547bf5341ee02394ab
GracielaCanessa <gracane@gmail.com>
2025-12-05 20:36:43 -0300

    Nuevas carpetas para la etoi261 generadas a partir de la rama "creo_etoi261#(creada por Alexis)

---
+++ etoi261/index.php
@@ -0,0 +1,5 @@
+<?php
+//UTF-8:SÍ
+header('Location: etoi261.php');
+
+?>
---
+++ operaciones_etoi261/dump_json_yeah.php
@@ -0,0 +1,76 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . "../etoi261");
+
+$nombre_app='etoi261';
+$NOMBRE_APP='etoi261';
+
+require_once "lo_imprescindible.php";
+
+$parametros_db->user='tedede_php';
+$parametros_db->pass='laclave';
+$parametros_db->base_de_datos='etoi261_produc_db';
+$parametros_db->port='5432';
+$parametros_db->host='10.35.200.10';
+
+require_once "pdo_con_excepciones.php";
+require_once "json_dumper.php";
+
+file_put_contents("empezo.tmp","Este archivo esta para ver a que hora empezo el json_dump");
+
+$jerarquia=array(
+'ua'=>'operativos',
+'con_opc'=>'operativos',
+'formularios'=>'operativos',
+'matrices'=>'formularios',
+'bloques'=>'matrices',
+'filtros'=>'bloques',
+'preguntas'=>'bloques',
+'variables'=>'preguntas',
+'opciones'=>'con_opc',
+'saltos'=>'variables',
+);
+
+$que_tablas=array('operativos');
+
+if("tambien poner otros metadatos importantes"){
+    $que_tablas=array_merge(
+        $que_tablas,
+        array(
+            'a_ingreso',
+            'con_momentos',
+            'diccionario',
+            'dicvar',
+            'dictra',
+            'dispositivo',
+            'dominio',
+            'estados_ingreso',
+            'importancia',
+            'planillas',
+            'replica',
+            'result_sup',
+            'roles', 
+            'tabulados',
+            'tipo_nov',
+            'verificado',
+            'volver_a_cargar'
+        )
+    );
+    $jerarquia+=array(
+        'baspro'=>'operativos',
+        'baspro_var'=>'baspro',
+        'consistencias'=>'operativos',
+        'estados'=>'operativos',
+        'est_rol'=>'estados', 
+        'est_var'=>'estados', 
+        'pla_est'=>'planillas',
+        'pla_var'=>'planillas',
+        'rol_pla'=>'roles',
+        'rol_rol'=>'roles',
+        'semanas'=>'operativos',
+        'varcal'=>'operativos',
+        'varcalopc'=>'varcal'
+    );
+}
+
+file_put_contents("yeah_dump.json",json_dump('encu',$que_tablas,$jerarquia));
+?>
