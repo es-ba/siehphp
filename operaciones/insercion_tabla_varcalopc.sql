84d7292be08f49ad4451efc68b1580983fdc94ca
Raquel Salva <raquelzssalva@gmail.com>
2025-12-19 19:10:18 -0300

    Agregar campo comentarios de varcalopc en el script de instalacion

--- operaciones/insercion_tabla_varcalopc.sql
+++ operaciones/insercion_tabla_varcalopc.sql
@@ -1,7 +1,7 @@
 INSERT INTO encu.varcalopc(
             varcalopc_ope, varcalopc_varcal, varcalopc_opcion, varcalopc_expresion_condicion, 
             varcalopc_etiqueta, varcalopc_tlg, varcalopc_expresion_valor, 
-            varcalopc_origen, varcalopc_orden)
+            varcalopc_origen, varcalopc_orden, varcalopc_comentarios)
     select dbo.ope_actual(), varcalopc_varcal, varcalopc_opcion, varcalopc_expresion_condicion, 
             varcalopc_etiqueta, /*CAMPOS_AUDITORIA*/, varcalopc_expresion_valor, 
-            varcalopc_origen, varcalopc_orden from encu_anterior.varcalopc;
+            varcalopc_origen, varcalopc_orden, varcalopc_comentarios from encu_anterior.varcalopc;
